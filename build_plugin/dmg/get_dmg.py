7114dba7f7a7e268585681bb2d140002deb40c8e
wieszheng <1512945835@qq.com>
2025-07-13 00:41:34 +0800

    Continue working

--- build_plugin/dmg/get_dmg.py
+++ build_plugin/dmg/get_dmg.py
@@ -27,7 +27,7 @@ def get_json():
     return """filename = '""" + dmgName + """'
 volume_name = '""" + dmgName + """.dmg'
 format = 'UDBZ'
-files = ['""" + app_dir + r"""\\build\\""" + appName + """.app', r'""" + app_dir + """\\build_plugin\dmg\潘高的小站.webloc']
+files = ['""" + app_dir + r"""/build/""" + appName + """.app', r'""" + app_dir + """/build_plugin\dmg\潘高的小站.webloc']
 symlinks = {'Applications': '/Applications'}
 icon_locations = {
     '""" + appName + """.app': (160, 120),
@@ -37,8 +37,8 @@ icon_locations = {
 window_rect = ((200, 200), (590, 416))
 icon_size = 60
 text_size = 12
-badge_icon = '""" + app_dir + r"""\static\\app.icns'
-background = '""" + app_dir + r"""\\build_plugin\dmg\bg.png'
+badge_icon = '""" + app_dir + r"""/static/app.icns'
+background = '""" + app_dir + r"""/build_plugin/dmg/bg.png'
 """
 
 
